73da0c570aaaa5c88e77e3ca115eedfab4d05aee
emilio <emilionicolaslobos16@gmail.com>
2022-11-16 17:01:51 -0300

    create playRound function

--- index.js
+++ index.js
@@ -5,3 +5,25 @@ function getComputerChoice() {
     return randomPick;
 };
 
+function playRound(playerSelection, computerSelection) {
+    getComputerChoice();
+    computerSelection = getComputerChoice();
+    if (playerSelection === computerSelection) {
+        return 'Tie!';
+    } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+        return 'You lose! Paper beats rock';
+    } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+        return 'You win! Paper beats rock';
+    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+        return 'You win! Rock beats Scissors';
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+        return 'You lose! Rock beats Scissors';
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        return 'You win! Scissors beats Paper';
+    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+        return 'You lose! Scissors beats Paper';
+    }
+}
+let playerSelection = 'Rock';
+let computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection))
